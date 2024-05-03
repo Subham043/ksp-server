3bc6f51b4925ad5bc6460ebf562600db20537cb6
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-05-03 17:16:27 +0530

    time being fix for images

--- src/modules/upload/upload.routes.ts
+++ src/modules/upload/upload.routes.ts
@@ -19,7 +19,7 @@ export async function uploadRoutes(app: FastifyInstance) {
       schema: {
         params: getUuidParamSchema,
       },
-      preHandler: app.verifyJwt,
+      // preHandler: app.verifyJwt,
     },
     sendImageStream
   );
